ca16aebccf162ff1cbd59fc0149516b05c1df7b5
a23hashusraf <a23hashusraf@inspedralbes.cat>
2025-03-11 09:46:17 +0100

    fisica creada del jugador

---
+++ Tr3-HashamH/Assets/BirdScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2D;
+    public float flapStrength;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) == true){
+            rigidbody2D.linearVelocity = Vector2.up * flapStrength;
+
+        }
+
+    }
+}
